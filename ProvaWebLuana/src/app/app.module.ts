76569a483b8eb4b10e213db929f81b726cfed40a
LuanaMora <luanamora32@gmail.com>
2019-04-12 19:23:00 -0300

    Adicionado Componentes e Material

---
+++ ProvaWebLuana/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { VeiculoComponent } from './cadastro/veiculo/veiculo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    VeiculoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
